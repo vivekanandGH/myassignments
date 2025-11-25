462239170c1dd796ad547cb423556a6fb0e26aa7
vivekanandGH <catchvivekanand@gmail.com>
2025-11-25 16:21:04 +0530

    Add files via upload

---
+++ day1/Primitive Datatypes.js
@@ -0,0 +1,23 @@
+var firstname = "vivek"
+console.log(firstname)
+console.log(typeof firstname)//string
+
+var mobile = 9894332902
+console.log(mobile)
+console.log(typeof mobile)//number
+
+var playwright = true
+console.log(playwright)
+console.log(typeof playwright)//boolean
+
+var bignumber = 13654654654654654654n
+console.log(bignumber)
+console.log(typeof bignumber)//BigInt
+
+var test
+console.log(test)
+console.log(typeof test);//undefined
+
+var testing=null
+console.log(testing)
+console.log(typeof testing);//null
